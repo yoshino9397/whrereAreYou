07e76e23b55b40b9abf64f1738d67b836db3c6f3
yoshino9397 <yayamayoshino@gmail.com>
2021-10-15 15:41:21 -0700

    kkk

---
+++ JS/story.js
@@ -0,0 +1,27 @@
+// function hoge(befimage,afimage){
+//   var elm = document.getElementById(befimage);
+//   elm.src = afimage;
+//   return
+// }
+// hoge("top","../IMAGE/topura.png");
+ 
+
+// function foo(befimage,afimage){
+//   var elm = document.getElementById(befimage);
+//   elm.src = afimage;
+//   return
+// }
+// foo("top","../IMAGE/top.png");
+
+// function hoge(){
+//   var elm = document.getElementById("top");
+//   elm.src = "../IMAGE/topura.png";
+  
+  
+// }
+ 
+// //マウスアウト時の処理を記述
+// function foo(){
+//   var elm = document.getElementById("top");
+//   elm.src = "../IMAGE/top.png";
+// }
