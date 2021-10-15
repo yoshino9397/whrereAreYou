6d5f9bf2dc83d24dc69b2348cd9fbb06b6207a61
yoshino9397 <yayamayoshino@gmail.com>
2021-10-14 22:10:12 -0700

    music

--- JS/header.js
+++ JS/header.js
@@ -1,13 +1,36 @@
-function header(){
+function header() {
   $.ajax({
-      url: '/Users/sam/Desktop/CodeFiles/whrereAreYou/HTML/header.html',
-      cache: false,
-      async: false,
-      dataType: 'html',
-      success: function(html){
-          document.write(html);
-      }
+    url: "/Users/sam/Desktop/CodeFiles/whrereAreYou/HTML/header.html",
+    cache: false,
+    async: false,
+    dataType: "html",
+    success: function (html) {
+      document.write(html);
+    },
   });
+}
+////////////---------BGM AutoLoop--------////////////////////////////////////
+window.addEventListener("DOMContentLoaded", function () {
+  var vid = document.getElementById("music");
+  vid.loop = true;
+  vid.load();
+
+  const audioElement = document.querySelector("music");
+
+  audioElement.addEventListener("loadeddata", (e) => {
+    audioElement.muted = true;
+    audioElement.autoplay = true;
+  });
+});
+//////////////----BGM ONOFF------//////////////////////////
+var music = document.getElementById("music");
+
+function enableMute() {
+  music.muted = true;
+}
+
+function disableMute() {
+  music.muted = false;
 }
 
 //<script>header();</script>
