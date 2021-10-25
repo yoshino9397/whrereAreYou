c089f9a17a26a8bf400d79cbde72aefc099bc5cb
yoshino9397 <yayamayoshino@gmail.com>
2021-10-24 17:46:09 -0700

    modalFin

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,3 +1,24 @@
-function modal(){
-
+function modal() {
+  document.getElementById("modal1").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff() {
+  document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal1").style.opacity = "0";
+}
+function modal2() {
+  document.getElementById("modal2").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff2() {
+  document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal2").style.opacity = "0";
+}
+function modal3() {
+  document.getElementById("modal3").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff3() {
+  document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal3").style.opacity = "0";
 }
