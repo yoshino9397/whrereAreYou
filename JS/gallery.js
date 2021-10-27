94b6bf32aa4de65f89711205ad5ae9d596a5eafd
yoshino9397 <yayamayoshino@gmail.com>
2021-10-27 16:11:19 -0700

    tuika

--- JS/gallery.js
+++ JS/gallery.js
@@ -22,3 +22,27 @@ function modaloff3() {
   document.getElementById("mask").style.opacity = "0";
   document.getElementById("modal3").style.opacity = "0";
 }
+function modal4() {
+  document.getElementById("modal4").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff4() {
+  document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal4").style.opacity = "0";
+}
+function modal5() {
+  document.getElementById("modal5").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff5() {
+  document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal5").style.opacity = "0";
+}
+function modal6() {
+  document.getElementById("modal6").style.opacity = "1";
+  document.getElementById("mask").style.opacity = "1";
+}
+function modaloff6() {
+  document.getElementById("mask").style.opacity = "0";
+  document.getElementById("modal6").style.opacity = "0";
+}
